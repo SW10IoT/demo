f4127180dfba9185c4ec141e1d7a1f2436f68aa3
Thalmann <mr.thalmann@gmail.com>
2016-06-02 11:24:54 +0200

    Moved ci to seperate file wuhuu pyt is awesome

--- demo.py
+++ demo.py
@@ -1,39 +1,10 @@
 from flask import render_template, Flask
+from command_injection import Grocery, save_grocery, reset_groceries, get_groceries
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
 
 
-################# COMMAND INJECTION ###############################
-from flask_wtf import Form
-from wtforms.fields import SubmitField, StringField
-import subprocess
-import os
-
-
-class Grocery(Form):
-    grocery_name = StringField('Grocery')
-    add_grocery = SubmitField('Add Grocery')
-    reset_groceries = SubmitField('Reset Groceries')
-
-def save_grocery(grocery_name):
-    command = 'echo ' + grocery_name + ' >> ' + ' groceries.txt'
-    subprocess.call(command, shell=True)
-
-def get_groceries():
-    filename = 'groceries.txt'
-    if os.path.isfile(filename):
-        with open(filename, 'r') as fd:
-            for line in fd:
-                yield line
-
-def reset_groceries():
-    filename = 'groceries.txt'
-    if os.path.isfile(filename):
-        os.remove(filename)
-
-
-################### ROUTES #########################
 @app.route('/')
 @app.route('/overview')
 def index():
