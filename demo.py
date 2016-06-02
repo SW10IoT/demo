0bde73fcccda9da63567f7f24ec596fbe9f86162
Thalmann <mr.thalmann@gmail.com>
2016-06-02 10:11:07 +0200

    Added command injection example

--- demo.py
+++ demo.py
@@ -1,10 +1,28 @@
 from flask import render_template, Flask
+from command_injection import Grocery, reset_groceries, get_groceries, save_grocery
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
+
 
 @app.route('/')
 @app.route('/overview')
 def index():
     return render_template('overview.html')
+
+@app.route('/command_injection', methods=['GET', 'POST'])
+def command_injection():
+    form = Grocery()
+    if form.validate_on_submit():
+        if form.reset_groceries.data:
+            reset_groceries()
+        elif form.grocery_name != '':
+            save_grocery(form.grocery_name.data)
+            form.grocery_name.data = ''
+            groceries = list(get_groceries())
+            return render_template('command_injection.html', form=form, groceries=groceries)
+    groceries = list(get_groceries())
+    return render_template('command_injection.html', form=form, groceries=groceries)
 
 if __name__ == '__main__':
     app.run(debug = True)
